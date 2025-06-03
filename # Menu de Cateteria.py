9154fedd8dc2ec157c2dd6ce5de849b272e7d760
Mikyrex24 <mickyleon7224@gmail.com>
2025-06-02 20:23:03 -0400

    Add files via upload

---
+++ # Menu de Cateteria.py
@@ -0,0 +1,20 @@
+# Menu de Cateteria
+ 
+print("Bienvenido a nuestra cafeteria.")
+print("1. Chocolata")
+print("2. Sandiwch de huevo")
+print("3. Porcion de papas fritas")
+
+orden = input("Que desea pedir?: ")
+
+if orden == "1":
+    print("Una chocolata. Muy buena seleccion!")
+ 
+elif orden == "2":
+    print("Deliciosa seleccion!")
+
+elif orden == "3":
+    print("Algo tranquilo pero delicioso!")
+
+else: 
+    print("Elige una de las opciones")
